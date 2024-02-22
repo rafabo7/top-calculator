eb4c9aa2a77f1fcc76627971a041f445afd5d58b
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-02-22 17:40:01 +0100

    Add comments to keep track of the funcionalities Create the operate() func that calls the funcs for the diferents operations.

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+// Math operations availables
 function add(a, b) {return a + b}
 function subs(a, b){return a - b}
 function div(a, b){return a/b}
 function mult(a, b){return a*b}
+
+// Operation function
+function operate(a, b, op) {
+    switch (op){
+        case "+":
+            return add(a, b)
+        case "-":
+            return subs(a, b)
+        case "/":
+            return div(a, b)
+        case "*":
+            return mult(a, b)
+    }
+}
 
 let a = 0
 let b = 0
