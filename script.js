f544d7d23e08572722b86b7e27ef417a8231703b
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-02-25 13:14:50 +0100

    Add one more operations and funcionality. Add the modulus function to the available operations. Add the DOM manipulation targets. To do so I tried the 'data-*' attribute for HTML buttons, it is suitable for this project, after a few adjustments it work. Add the event listener for the numbers, the operations and the equal button with forEach() if suitable. The correct display of the opertions still have work.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function add(a, b) {return a + b}
 function subs(a, b){return a - b}
 function div(a, b){return a/b}
 function mult(a, b){return a*b}
+function mod(a, b){return a%b}
 
 // Operation function
 function operate(a, b, op) {
@@ -15,10 +16,39 @@ function operate(a, b, op) {
             return div(a, b)
         case "*":
             return mult(a, b)
+        case "%":
+            return mod(a, b)
     }
 }
 
-let a = 0
-let b = 0
+let a = ''
+let b = ''
 let op = ''
 
+const display = document.querySelector('#screen')
+const numbers = document.querySelectorAll('[data-number]')
+const operation = document.querySelectorAll('[data-operation]')
+const equals = document.querySelector('#equals')
+
+numbers.forEach(button => button.addEventListener('click', () => {
+    if (!op) {
+        display.textContent += button.textContent
+        a += button.textContent
+    }
+    if (op) {
+        display.textContent += button.textContent
+        b += button.textContent
+    }
+}))
+
+operation.forEach(button => button.addEventListener ('click', () => {
+    display.textContent += ` ${button.textContent} `
+    op = button.textContent
+}))
+
+equals.addEventListener('click', () => {
+    a = Number(a)
+    b = Number(b)
+    let result = operate(a, b, op)
+    display.textContent = result
+})
